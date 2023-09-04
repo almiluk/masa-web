03b9260c54068065a084c1b369acbbd111675e61
Aleksandr Lukianov <32066686+almiluk@users.noreply.github.com>
2023-09-04 23:10:20 +0300

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,1 +1,1 @@
-lftp -e "set ssl:check-hostname no; mirror -R --only-newer -F tasks/*; bye;" -u $LFTP_USER,$LFTP_PASSWORD $LFTP_HOST
+lftp -e "set ftp:ssl-allow no; set ssl:check-hostname no; mirror -R --only-newer -F tasks/*; bye;" -u $LFTP_USER,$LFTP_PASSWORD $LFTP_HOST
