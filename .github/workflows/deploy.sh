13dc0da35f2702e549b2ab31b35433dcb7615ebf
almiluk <almiluk@gmail.com>
2023-09-04 22:42:07 +0300

    fix actions

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,3 +1,4 @@
 echo $HOME
 echo $LFTP_HOST
-lftp -e "set ssl:check-hostname no; mirror -R --only-newer -F ../../tasks/*; bye;" -u $LFTP_USER,$LFTP_PASSWORD $LFTP_HOST
+lftp -e "set ssl:check-hostname no; mirror -R --only-newer -F tasks/*; bye;" -u $LFTP_USER,$LFTP_PASSWORD $LFTP_HOST
+
